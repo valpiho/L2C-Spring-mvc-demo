3be3d9c771a0c6e64891cfe30e31b67edcab50ba
Valentin Piho <val.piho@gmail.com>
2020-10-07 19:28:27 +0300

    Custom validation message + other validations

--- src/main/java/com/pibox/springmvcdemo/SpringMvcDemoApplication.java
+++ src/main/java/com/pibox/springmvcdemo/SpringMvcDemoApplication.java
@@ -2,12 +2,27 @@ package com.pibox.springmvcdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class SpringMvcDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcDemoApplication.class, args);
+    }
+
+    @Configuration
+    public static class Config {
+
+        @Bean
+        public MessageSource messageSource () {
+            ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+            messageSource.setBasenames("messages");
+            return messageSource;
+        }
     }
 
 }
